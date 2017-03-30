e1441094d0fa5a1560c7fd00a1600b15c5231619
Emmanuel Olowosulu <eolowo@unilag.edu.ng>
2017-03-30 13:41:55 +0100

    Fixed error
    Signed-off-by: Emmanuel Olowosulu <eolowo@unilag.edu.ng>

--- dbo.js
+++ dbo.js
@@ -3,7 +3,7 @@ module.exports = function (opts) {
   .connect(process.env.DB_URL, { db: { safe: { w: 1, journal: true, wtimeout: 10000 } }, config: { autoIndex: true } });
   opts.mongoose.connection.on('error', function mongooseConnected() {
     opts.log.error('Mongoose connection error: plese verify that mongod is running');
-    opts.log.error(error);
+    //opts.log.error(error);
   });
   opts.mongoose.connection.once('open', function mongooseOpened() {
     opts.log.info('Mongoose connected to the database');
