7c147a59bfd6109f598b3a66350595120b260b79
Emmanuel Olowosulu <eolowo@unilag.edu.ng>
2017-03-30 09:15:46 +0100

    Initial Theme Layout
    Signed-off-by: Emmanuel Olowosulu <eolowo@unilag.edu.ng>

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/auth', function(req, res, next) {
+  res.render('auth', { title: 'Express' });
+});
+
 module.exports = router;
